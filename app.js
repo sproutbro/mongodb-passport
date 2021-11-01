0966efc5f7fea56e4f0002359ced2f8e2c75c8d1
sprout <krsproutbro@gmail.com>
2021-11-02 00:02:19 +0900

    update node_env

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
-require("dotenv").config()
+if(process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
 
 app.use(express.static("public"))
 app.use(express.json())
